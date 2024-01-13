7a0b588156146cb3afd0b75aa5933b6af74e5622
sohag <shishirsohagislam@gmail.com>
2024-01-13 22:52:35 +0600

    tailwind custom class added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <Container>
      <div className="h-screen w-full flex justify-center items-center">
-    <button className="px-3 py-2 bg-purple-400 rounded-md">This is a button</button> 
+    <button className="btn btn-primary">This is a button</button> 
      </div>
     </Container>
   )
