a62e1a751cc9e5063bb685719c55ee2c4894c4cf
sohag <shishirsohagislam@gmail.com>
2024-01-13 21:56:24 +0600

    container component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import MainLayout from "./components/layout/MainLayout"
-
+import Container from "./components/ui/Container"
 function App() {
-
-  return  <MainLayout/>
+  return (
+    <Container>
+     <div className="h-screen w-full flex justify-center items-center">
+    <button className="px-3 py-2 bg-purple-400 rounded-md">This is a button</button> 
+     </div>
+    </Container>
+  )
 }
 export default App
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,7 +4,6 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import AdminLayout from "../components/layout/AdminLayout";
 
-
 const router = createBrowserRouter([
     {
         path:'/',
