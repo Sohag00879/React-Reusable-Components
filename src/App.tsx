21ab6efcdc068d5a8df5e7132163905a7d4a41ab
sohag <shishirsohagislam@gmail.com>
2024-01-14 16:13:48 +0600

    solution of merge problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Button from "./components/ui/Button"
 import Container from "./components/ui/Container"
 function App() {
   return (
     <Container>
      <div className="h-screen w-full flex justify-center items-center">
-    <button className="btn btn-primary">This is a button</button> 
+    <Button outline={true}/>
      </div>
     </Container>
   )
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,12 @@
+import cn from "../../utils/cn"
+const Button = ({className, outline}) => {
+  return <button className={cn(
+    "bg-purple-500 px-3 py-2 rounded-md", 
+    {
+        "border border-purple-500 bg-opacity-10" : outline,
+    },
+    className
+  )}>Click</button>
+}
+
+export default Button
